16e46c62268c22a7ff861179a5dfbfa5e70e3b12
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-05-05 19:13:22 -0700

    Add some styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import './app.css';
 function App() {
   return (
     <div className='App'>
-      <div className='main'></div>
+      <div className='main'>Main</div>
+      <div className='pyramid'>
+        <ul className='moneyList'>
+          <li className='moneyListItem'>$ 400</li>
+          <li className='moneyListItem'>$ 300</li>
+          <li className='moneyListItem'>$ 200</li>
+          <li className='moneyListItem'>$ 100</li>
+        </ul>
+      </div>
     </div>
   );
 }
