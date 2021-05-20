6bb7c51da036af557e6e531983141e7a808e45ff
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-05-19 20:21:18 -0700

    Add the sounds

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import useSound from 'use-sound';
 import './questions.css';
+import play from '../assets/src_sounds_play.mp3';
+import correct from '../assets/src_sounds_correct.mp3';
+import wrong from '../assets/src_sounds_wrong.mp3';
 
 export default function Questions({
   data,
@@ -10,6 +14,13 @@ export default function Questions({
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState('answer');
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
@@ -27,12 +38,18 @@ export default function Questions({
     delay(3000, () =>
       setClassName(a.correct ? 'answer correct' : 'answer wrong')
     );
-    delay(6000, () => {
+    delay(5000, () => {
       if (a.correct) {
-        setQuestionNumber(prev => prev + 1);
-        setSelectedAnswer(null);
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber(prev => prev + 1);
+          setSelectedAnswer(null);
+        });
       } else {
-        setStop(true);
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
       }
     });
   };
